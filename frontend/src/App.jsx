a230274d73477b59880c28b0033ee8285d1c3d14
Codemaico <devwithmiki@gmail.com>
2025-09-19 15:03:58 +0200

    now the home page loads for /movie-app route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/movie-app" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
